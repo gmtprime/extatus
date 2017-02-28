74409e03419e4cd1324ae8293895f764d3825d1c
Alexander De Sousa <alexander.jose.de.sousa@gmail.com>
2017-02-28 16:40:21 +0100

    Prepared mix file for publication

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Extatus.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :extatus,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     docs: docs(),
      deps: deps()]
   end
 
@@ -23,5 +28,23 @@ defmodule Extatus.Mixfile do
      {:uuid, "~> 1.1", only: [:dev, :test]},
      {:inch_ex, "~> 0.5", only: [:dev, :test]},
      {:credo, "~> 0.6", only: [:dev, :test]}]
+  end
+
+  defp docs do
+    [source_url: "https://github.com/gmtprime/extatus",
+     source_ref: "v#{@version}",
+     main: Extatus]
+  end
+
+  defp description do
+    """
+    App to report metrics to Prometheus from Elixir GenServers.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Alexander de Sousa"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/gmtprime/extatus"}]
   end
 end
